8a7656cbc9ec4978ae2993659e2d013bc91ca03c
moinulhossainmahim <moinulhossainmahim@gmail.com>
2021-09-15 20:38:18 +0600

    set showing question with changing and checking functionalities

--- src/App.js
+++ src/App.js
@@ -5,7 +5,16 @@ import SetupForm from './SetupForm'
 import Loading from './Loading'
 import Modal from './Modal'
 function App() {
-  const {isWaiting, isLoading, error, questions, index, correct} = useGlobalContext()
+  const {
+    isWaiting,
+    isLoading,
+    error,
+    questions,
+    index,
+    correct,
+    nextQuestion,
+    checkAnswer
+  } = useGlobalContext()
   if (isWaiting) {
     return <SetupForm />
   }
@@ -13,9 +22,37 @@ function App() {
   if (isLoading) {
     return <Loading/>
   }
-  return <main>
-    quiz app
-  </main>
+
+  const {question, correct_answer, incorrect_answers} = questions[index]
+  const answers = [ ...incorrect_answers, correct_answer]
+
+  return (
+    <main>
+      <section className='quiz'>
+        <p className="correct-answers">
+          correct answers : {correct} / {index}
+        </p>
+        <article className='container'>
+          <h2 dangerouslySetInnerHTML={{__html: question}} />
+          <div className="btn-container">
+            {answers.map((answer, index) => {
+              return (
+                <button
+                  key={index}
+                  className='answer-btn'
+                  onClick={() => checkAnswer(answer === correct_answer)}
+                  dangerouslySetInnerHTML={{__html: answer}}
+                />
+              )
+            })}
+          </div>
+        </article>
+        <button className='next-question' onClick={nextQuestion}>
+          next question
+        </button>
+      </section>
+    </main>
+  )
 }
 
 export default App
--- src/context.js
+++ src/context.js
@@ -23,6 +23,48 @@ const AppProvider = ({ children }) => {
   const [error, setError] = useState(false)
   const [isModalOpen, setIsModalOpen] = useState(false)
 
+  const fetchQuestions = async (url) => {
+    setIsLoading(true)
+    setIsWaiting(false)
+    const response = await axios(url).catch((error) => console.log(error))
+    if (response) {
+      const data = response.data.results
+      if (data.length > 0) {
+        setQuestions(data)
+        setIsWaiting(false)
+        setIsLoading(false)
+        setError(false)
+      } else {
+        setIsWaiting(true)
+        setError(true)
+      }
+    } else {
+      setIsWaiting(true)
+    }
+  }
+
+  
+  const nextQuestion = () => {
+    setIndex((prevIndex) => {
+      let index = prevIndex + 1
+      if (index > questions.length - 1) {
+        return 0
+      }
+      return index
+    })
+  }
+  
+  const checkAnswer = (value) => {
+    if (value) {
+      setCorrect((prevState) => prevState + 1)
+    }
+    nextQuestion()
+  }
+
+  useEffect(() => {
+    fetchQuestions(tempUrl)
+  }, [])
+
   return <AppContext.Provider value={{
     isWaiting,
     isLoading,
@@ -30,7 +72,9 @@ const AppProvider = ({ children }) => {
     index,
     error,
     correct,
-    isModalOpen
+    isModalOpen,
+    nextQuestion,
+    checkAnswer
   }}>{children}</AppContext.Provider>
 }
 
